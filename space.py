4bbd6dedc8f3bdcc281af138519240de536853ac
evanski14 <killmasterjoy13@live.com>
2017-09-08 05:34:14 +0200

    Just add a spaceship \o/

--- space.py
+++ space.py
@@ -9,9 +9,14 @@ class SpaceGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        self.ship = arcade.Sprite('Desktop/spaceship.png')
+        self.ship.set_position(100,100)
+
 
     def on_draw(self):
         arcade.start_render()
+
+        self.shop.draw()
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
